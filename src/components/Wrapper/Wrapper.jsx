0e72bccdb64d341abed33e62cbc66d91ff062d14
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-09-04 08:45:11 +0300

    add Wrapper refactoring with hooks

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -1,63 +1,85 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
 import Notification from '../Notification/Notification';
 import Section from '../Section/Section';
 import Statistics from '../Statistics/Statistics';
 
-class Wrapper extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+function Wrapper() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodUpdate = () => {
+    setGood(prev => prev + 1);
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, el) => acc + el, 0);
+  const handleBadUpdate = () => {
+    setBad(prev => prev + 1);
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+  const handleNeutralUpdate = () => {
+    setNeutral(prev => prev + 1);
+  };
+
+  const state = { good, neutral, bad };
+
+  const countTotalFeedback = () => {
+    return Object.values(state).reduce((acc, el) => acc + el, 0);
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
 
     return Math.round((good / total) * 100);
   };
 
-  onLeaveFeedbackHandler = option => {
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1,
-    }));
+  const onLeaveFeedbackHandler = option => {
+    console.log(option);
+
+    switch (option) {
+      case 'good':
+        handleGoodUpdate();
+        break;
+
+      case 'neutral':
+        handleBadUpdate();
+        break;
+
+      case 'bad':
+        handleNeutralUpdate();
+        break;
+      default:
+        console.warn('not supported');
+    }
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-    const options = Object.keys(this.state);
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+  const options = Object.keys(state);
 
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.onLeaveFeedbackHandler}
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={onLeaveFeedbackHandler}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-        <Section title="Statistics">
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          ) : (
-            <Notification message="No feedback given" />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        ) : (
+          <Notification message="No feedback given" />
+        )}
+      </Section>
+    </>
+  );
 }
 
 export default Wrapper;
